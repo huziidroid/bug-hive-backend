5d355bd8aac79a1a2224650c6bf24da102c7d0ad
Huzaifa-Personal <huzaifa.arshad1911@gmail.com>
2023-08-30 22:19:34 +0500

    Formatted Reporter controller file

--- src/modules/reporters/reporters.controller.ts
+++ src/modules/reporters/reporters.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param, Query } from '@nestjs/common';
+import { Controller, Get, Param, Post, Query } from '@nestjs/common';
 
 import { ReportersService } from './reporters.service';
 import { FindAllQueryParams } from './dtos';
